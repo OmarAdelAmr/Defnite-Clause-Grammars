64301e25487e3bed4e4af61389fb7567e82d7e80
Omar Adel <omaradel9545@gmail.com>
2017-04-09 23:15:03 +0200

    [ADD] all working

--- Code.pl
+++ Code.pl
@@ -1,15 +1,27 @@
 sentence(sentence(NP,VP))                    --> noun_phrase(NP), verb_phrase(VP). 
-% A sentence consist of a noun phrase and a verb phrase.
 
 
-noun_phrase(noun_phrase(D, N))               --> det(D, B), noun(B, N).
+noun_phrase(noun_phrase(D, N))                    			 --> det(D), noun(N).
+noun_phrase(noun_phrase(D, N, Prep, NP))                     --> det(D), noun(N), prep(Prep), noun_phrase(NP).
+noun_phrase(noun_phrase(D, Adj, N))               			 --> det(D), adj(Adj), noun(N).
+noun_phrase(noun_phrase(D, Adj, N, C, VP))               	 --> det(D), adj(Adj), noun(N), conj(C), verb_phrase(VP).
+noun_phrase(noun_phrase(D, Adj1, Adj2, N))        			 --> det(D), adj(Adj1), adj(Adj2), noun(N).
+noun_phrase(noun_phrase(D, Adj, N, Prep, NP))                --> det(D), adj(Adj), noun(N), prep(Prep), noun_phrase(NP).
+noun_phrase(noun_phrase(D, Adj, N, Prep, NP, Prep2, N2))     --> det(D), adj(Adj), noun(N), prep(Prep)
+    														 , noun_phrase(NP), prep(Prep2), noun(N2).
 
-verb_phrase(verb_phrase(V))                  --> verb(V).                      % A verb phrase can be a transitive verb
 
+verb_phrase(verb_phrase(V))                  --> verb(V).                   
+verb_phrase(verb_phrase(V, NP))              --> verb(V), noun_phrase(NP).
+verb_phrase(verb_phrase(V, NP1, NP2))        --> verb(V), noun_phrase(NP1), noun_phrase(NP2).
+verb_phrase(verb_phrase(V, Prep,NP))         --> verb(V), prep(Prep), noun_phrase(NP).
+verb_phrase(verb_phrase(V, Prep,NP, Adv))    --> verb(V), prep(Prep), noun_phrase(NP), adv(Adv).
+verb_phrase(verb_phrase(V, Obj, Det, N))     --> verb(V), obj(Obj), det(Det), noun(N).
+verb_phrase(verb_phrase(Adv, V, NP))     	 --> adv(Adv), verb(V), noun_phrase(NP).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Words 
+% Definitions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 noun(noun(boy))                             --> [boy].
@@ -23,6 +35,8 @@ noun(noun(shed))                            --> [shed].
 noun(noun(building))                        --> [building].
 noun(noun(letters))                         --> [letters].
 noun(noun(tree))                            --> [tree].
+
+obj(obj(me)) --> [me].
 
 
 verb(verb(put))           --> [put].
@@ -47,6 +61,8 @@ adj(adj(poor))        --> [poor].
 adj(adj(white))       --> [white].
 adj(adj(pretty))      --> [pretty].
 
+conj(conj(who))      --> [who].
+
 
 adv(adv(secretly))    --> [secretly].
 adv(adv(quickly))     --> [quickly].
@@ -64,17 +80,16 @@ det(det(every))        --> [every].
 det(det(some))         --> [some].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-/*
+/**
 
-% "mi moku." has two different parse trees here (I eat. / I am food.).
-?- sentence(Parse_tree, [mi,moku], []).
-Parse_tree = sentence(noun_phrase(pronoun(mi)), verb_phrase(verb(moku)), separator('.')) ;
-Parse_tree = sentence(noun_phrase(pronoun(mi)), verb_phrase(verb_be(''), noun(moku)), separator('.')) ;
+1- sentence(Parse_tree, [the, old, boy, put, a , big, box, in, the, large, empty, room, after, school], []).
+
+2- sentence(Parse_tree, [the, old, woman, gave, the, poor, man, a, 
+	white, envelope, in, the, shed, behind, the, building], []).
 
 
---------------------------------
+3- sentence(Parse_tree, [the, pretty, woman, who, worked, with, the, old, man, secretly, gave, me, three, letters], []).
 
-% List the structure:
-?- listing.
+4- sentence(Parse_tree, [every, boy, quickly, climbed, some, big, tree], []).
 
 */
